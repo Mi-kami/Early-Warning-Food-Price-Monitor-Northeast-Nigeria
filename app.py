1e7cc6684b8b391e08994c843193cf991094da4f
Olofin Deborah <deboraholofin43@gmail.com>
2026-03-12 00:44:46 +0100

    Update app.py

--- app.py
+++ app.py
@@ -6,10 +6,10 @@ st.title("Early Warning Food Price Monitor - Northeast Nigeria")
 st.write("This tool forecasts retail food prices three months ahead for five key commodities across Adamawa, Borno, and Yobe — three conflict-affected states in Northeast Nigeria where food insecurity is most acute. Three months of advance warning is not just a number. It is enough time for policymakers, traders, aid organisations and food security agencies to see what is coming and act before the crisis arrives.")
 
 #reading in the needed datasets
-model_comparison = pd.read_csv('data/model data/model_comparison_final.csv')
-best_models = pd.read_csv('data/model data/best_model_per_combination.csv')
-prophet_horizons = pd.read_csv('data/model data/prophet_horizon_results.csv')
-xgb_horizons = pd.read_csv('data/model data/xgb_horizon_results.csv')
+model_comparison = pd.read_csv('data/model_data/model_comparison_final.csv')
+best_models = pd.read_csv('data/model_data/best_model_per_combination.csv')
+prophet_horizons = pd.read_csv('data/model_data/prophet_horizon_results.csv')
+xgb_horizons = pd.read_csv('data/model_data/xgb_horizon_results.csv')
 
 # creating sidebar for navigation
 page = st.sidebar.selectbox(
